1aada5975e93efcc80b98233e01c11b74344eb5b
Evgeniy Kukarskiy <jon.79555@gmail.com>
2024-10-13 17:53:14 +0700

    Поправил. Решил проблему с создание обоих бинов через аннотацию @Primary

--- src/main/java/ru/netology/springBootHW/config/JavaConfig.java
+++ src/main/java/ru/netology/springBootHW/config/JavaConfig.java
@@ -3,6 +3,7 @@ package ru.netology.springBootHW.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import ru.netology.springBootHW.profiles.DevProfile;
 import ru.netology.springBootHW.profiles.ProductionProfile;
 import ru.netology.springBootHW.profiles.SystemProfile;
@@ -11,12 +12,12 @@ import ru.netology.springBootHW.profiles.SystemProfile;
 public class JavaConfig {
 
     @ConditionalOnProperty("netology.profile.dev")
+    @Primary
     @Bean
     public SystemProfile devProfile() {
         return new DevProfile();
     }
 
-    @ConditionalOnProperty("netology.profile.prod")
     @Bean
     public SystemProfile prodProfile() {
         return new ProductionProfile();
